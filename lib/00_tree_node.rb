0806f1ead815de851ca61ed85d8dec0fa285238b
mbegur <mbegur@gmail.com>
2017-06-02 12:26:55 -0700

    add add_child method

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -15,4 +15,17 @@ class PolyTreeNode
     parent_node.children << self unless parent_node.children.include?(self)
   end
 
+  def add_child(child_node)
+    @children << child_node
+    child_node.parent= self
+  end
+
+  def remove_child(child_node)
+  end
+
 end
+
+
+# n1 = PolyTreeNode.new("root1")
+# n2 = PolyTreeNode.new("root2")
+# n3 = PolyTreeNode.new("root3")
